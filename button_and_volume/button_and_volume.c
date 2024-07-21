bfc7deb0f32f485c6601fb77520148fc3e5fdac9
Saki Sakira <sakisakira@gmail.com>
2024-07-21 12:02:29 +0900

    Enabled to output 'A' keycode when on-board button is pressed.

--- button_and_volume/button_and_volume.c
+++ button_and_volume/button_and_volume.c
@@ -1,23 +1,85 @@
+#include <stdlib.h>
 #include <stdio.h>
-#include "pico/stdlib.h"
+#include <string.h>
+
+#include "bsp/board.h"
+#include "tusb.h"
+
+#include "usb_descriptors.h"
+
 #include "hardware/gpio.h"
 #include "pico/binary_info.h"
 
-const uint LED_PIN = 25;
+void hid_task(void);
+static void send_hid_report(uint32_t button);
 
 int main() {
-  bi_decl(bi_program_description("This is a test binary."));
-  bi_decl(bi_1pin_with_name(LED_PIN, "On-board LED"));
+  board_init();
+  tusb_init();
 
-  stdio_init_all();
+  while (1)
+  {
+    tud_task();
 
-  gpio_init(LED_PIN);
-  gpio_set_dir(LED_PIN, GPIO_OUT);
-  while (1) {
-    gpio_put(LED_PIN, 0);
-    sleep_ms(250);
-    gpio_put(LED_PIN, 1);
-    puts("Hello World\n");
-    sleep_ms(1000);
+    hid_task();
   }
+
+  return 0;
+}
+
+void hid_task(void) {
+  const uint32_t interval_ms = 10;
+  static uint32_t start_ms = 0;
+
+  if (board_millis() - start_ms < interval_ms) return;
+  start_ms += interval_ms;
+
+  uint32_t const button = board_button_read();
+
+  if (tud_suspended() && button)
+  {
+    tud_remote_wakeup();
+  } else {
+    send_hid_report(button);
+  }
+}
+
+static void send_hid_report(uint32_t button) {
+  if (!tud_hid_ready()) return;
+  
+  static bool has_keyboard_key = false;
+  if (button) {
+    uint8_t keycode[6] = {0};
+    keycode[0] = HID_KEY_A;
+
+    tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0 , keycode);
+    has_keyboard_key = true;
+  } else {
+    if (has_keyboard_key) 
+      tud_hid_keyboard_report(REPORT_ID_KEYBOARD, 0, NULL);
+      has_keyboard_key = false;
+  }
+}
+
+// callback
+uint16_t tud_hid_get_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t* buffer, uint16_t reqlen)
+{
+  // TODO not Implemented
+  (void) instance;
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) reqlen;
+
+  return 0;
+}
+
+void tud_hid_set_report_cb(uint8_t instance, uint8_t report_id, hid_report_type_t report_type, uint8_t const* buffer, uint16_t bufsize) 
+{
+    // TODO not Implemented
+  (void) instance;
+  (void) report_id;
+  (void) report_type;
+  (void) buffer;
+  (void) bufsize;
 }
