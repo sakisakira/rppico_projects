a05f714a21ec171f4a6477a45cfe4dcf853fe77a
Saki Sakira <sakisakira@gmail.com>
2025-06-22 11:08:24 +0900

    Implemented to play MP3 sounds in microSD.

--- UniTaruBoard/UniTaruBoard.cpp
+++ UniTaruBoard/UniTaruBoard.cpp
@@ -1,5 +1,63 @@
 #include <stdio.h>
 #include "pico/stdlib.h"
+#include "lib/pico-dfPlayerMini/dfPlayer/dfPlayer.h"
+
+#include <vector>
+
+class DfPlayerPicoSd : public DfPlayer<DfPlayerPicoSd>
+{
+public:
+    DfPlayerPicoSd()
+    {
+        // Initialize UART 1
+        uart_init(uart1, 9600);
+
+        // Set the GPIO pin mux to the UART - 8 is TX, 9 is RX
+        gpio_set_function(8, GPIO_FUNC_UART);
+        gpio_set_function(9, GPIO_FUNC_UART);
+
+        sendCmd(0x3f, 0x0002); // Set to use only SD card.
+    }
+
+    inline void uartSend(uint8_t* a_cmd)
+    {
+        uart_write_blocking(uart1, a_cmd, SERIAL_CMD_SIZE);
+    }
+
+    inline std::vector<uint8_t> uartRead()
+    {
+        std::vector<uint8_t> data;
+        if (!uart_is_readable(uart1)) return data;
+
+        data.resize(sizeof(SERIAL_CMD_SIZE));
+        uart_read_blocking(uart1, data.data(), SERIAL_CMD_SIZE);
+        return data;
+    }
+
+    uint getSoundCount()
+    {
+        sendCmd(0x47, 0x0000);
+        while (!uart_is_readable(uart1)) {
+            sleep_ms(100); // Wait for the response
+        }
+        const std::vector<uint8_t> response = uartRead();
+        if (response.size() < SERIAL_CMD_SIZE) {
+            printf("Not enough data received\n");
+            return 0; // Not enough data received
+        }
+        return response[dfPlayer::serialCommFormat::PARA1] << 8 | response[dfPlayer::serialCommFormat::PARA2];
+    }
+
+    void playSound(uint8_t soundNumber)
+    {
+        constexpr uint8_t folder_index = 0;
+        sendCmd(dfPlayer::cmd::SPECIFY_FOLDER_PLAYBACK, 
+            folder_index << 8 | soundNumber);
+    }
+
+private:
+    static constexpr uint8_t SERIAL_CMD_SIZE = 10;
+};
 
 const uint LED_PIN = PICO_DEFAULT_LED_PIN;
 const uint ButtonRow0 = 2;
@@ -65,7 +123,10 @@ int main()
 {
     setup();
 
+    DfPlayerPicoSd player;
+
     while (true) {
+        // Key input handling.
         const uint code = scan_matrix();
         for (uint index = 0; index < code; ++index) {
             gpio_put(LED_PIN, true);
@@ -75,7 +136,19 @@ int main()
         }
         if (code > 0) {
             printf("Button pressed: %u\n", code);
+            player.playSound(code);
         }
+
+        // Display results of player operations.
+        const std::vector<uint8_t> data = player.uartRead();
+        if (!data.empty()) {
+            printf("Received data: ");
+            for (const auto& byte : data) {
+                printf("%02x ", byte);
+            }
+            printf("\n");
+        }
+
         gpio_put(LED_PIN, false);
         sleep_ms(100);
     }
